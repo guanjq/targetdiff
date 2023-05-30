d1dd3f310f4fc84e1132fd8aaa9d8efe2dc58477
Jiaqi Guan <jiaqiguan@bytedance.com>
2023-05-30 12:20:22 -0700

    update sample for pocket

--- scripts/sample_for_pocket.py
+++ scripts/sample_for_pocket.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 import shutil
-from glob import glob
 
 import torch
 from torch_geometric.transforms import Compose
@@ -12,6 +11,8 @@ from datasets.pl_data import ProteinLigandData, torchify_dict
 from models.molopt_score_model import ScorePosNet3D
 from scripts.sample_diffusion import sample_diffusion_ligand
 from utils.data import PDBProtein
+from utils import reconstruct
+from rdkit import Chem
 
 
 def pdb_to_pocket_data(pdb_path):
@@ -37,6 +38,7 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=str, default='cuda:0')
     parser.add_argument('--batch_size', type=int, default=100)
     parser.add_argument('--result_path', type=str, default='./outputs_pdb')
+    parser.add_argument('--num_samples', type=int)
     args = parser.parse_args()
 
     logger = misc.get_logger('evaluate')
@@ -70,8 +72,10 @@ if __name__ == '__main__':
     # Load pocket
     data = pdb_to_pocket_data(args.pdb_path)
     data = transform(data)
+    if args.num_samples:
+        config.sample.num_samples = args.num_samples
 
-    pred_pos, pred_v, pred_pos_traj, pred_v_traj, pred_v0_traj, pred_vt_traj, time_list = sample_diffusion_ligand(
+    all_pred_pos, all_pred_v, pred_pos_traj, pred_v_traj, pred_v0_traj, pred_vt_traj, time_list = sample_diffusion_ligand(
         model, data, config.sample.num_samples,
         batch_size=args.batch_size, device=args.device,
         num_steps=config.sample.num_steps,
@@ -81,14 +85,45 @@ if __name__ == '__main__':
     )
     result = {
         'data': data,
-        'pred_ligand_pos': pred_pos,
-        'pred_ligand_v': pred_v,
+        'pred_ligand_pos': all_pred_pos,
+        'pred_ligand_v': all_pred_v,
         'pred_ligand_pos_traj': pred_pos_traj,
         'pred_ligand_v_traj': pred_v_traj
     }
     logger.info('Sample done!')
 
+    # reconstruction
+    gen_mols = []
+    n_recon_success, n_complete = 0, 0
+    for sample_idx, (pred_pos, pred_v) in enumerate(zip(all_pred_pos, all_pred_v)):
+        pred_atom_type = trans.get_atomic_number_from_index(pred_v, mode='add_aromatic')
+        try:
+            pred_aromatic = trans.is_aromatic_from_index(pred_v, mode='add_aromatic')
+            mol = reconstruct.reconstruct_from_generated(pred_pos, pred_atom_type, pred_aromatic)
+            smiles = Chem.MolToSmiles(mol)
+        except reconstruct.MolReconsError:
+            gen_mols.append(None)
+            continue
+        n_recon_success += 1
+
+        if '.' in smiles:
+            gen_mols.append(None)
+            continue
+        n_complete += 1
+        gen_mols.append(mol)
+    result['mols'] = gen_mols
+    logger.info('Reconstruction done!')
+    logger.info(f'n recon: {n_recon_success} n complete: {n_complete}')
+
     result_path = args.result_path
     os.makedirs(result_path, exist_ok=True)
     shutil.copyfile(args.config, os.path.join(result_path, 'sample.yml'))
     torch.save(result, os.path.join(result_path, f'sample.pt'))
+    mols_save_path = os.path.join(result_path, f'sdf')
+    os.makedirs(mols_save_path, exist_ok=True)
+    for idx, mol in enumerate(gen_mols):
+        if mol is not None:
+            sdf_writer = Chem.SDWriter(os.path.join(mols_save_path, f'{idx:03d}.sdf'))
+            sdf_writer.write(mol)
+            sdf_writer.close()
+    logger.info(f'Results are saved in {result_path}')
